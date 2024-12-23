6ca8edadd1fe672fdb56fafc5403145dd7523415
priyanka56571 <priyankathulkar123@gmail.com>
2024-12-23 17:25:21 +0530

    my first commit

---
+++ default.js
@@ -0,0 +1,6 @@
+var age = 45;
+var data = "javascript";
+function display(){
+    alert("hello everyone");
+}
+export default display;
---
+++ named.js
@@ -0,0 +1,5 @@
+var name = "priyanka";
+function addition (a, b) {
+    return a + b;
+}
+export {name,addition};
